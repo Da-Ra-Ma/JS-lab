4fc62dcf02c58e66196341bc9bb45e2d800bc3ec
Da-Ra-Ma <danmed555@gmail.com>
2024-10-10 17:09:15 +0300

    Add JS-lab

---
+++ Lab_1/skript.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("id-button");
+const paragraph = document.querySelector("p");
+let buttonState = 0;
+
+let changeCssAttributes = function()
+{
+    if (buttonState === 0) 
+    {
+        paragraph.style.color = "red";
+        paragraph.style.fontSize = "100px";
+        paragraph.style.paddingLeft = "250px"
+
+        paragraph.innerText = "World hello!";
+
+        buttonState = 1;
+    }
+    else
+    {
+        paragraph.style.color = "black";
+        paragraph.style.fontSize = "50px";
+        paragraph.style.paddingLeft = "0px"
+
+        paragraph.innerText = "Hello world!";
+
+        buttonState = 0;
+    }
+}
+
+button.addEventListener("click", changeCssAttributes);
